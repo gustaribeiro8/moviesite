ec911aa4a31c2c71620df5f1d144b3c382dd5889
Gustavo Ribeiro <gustavo.ribeiro10@usp.br>
2023-11-14 08:15:08 -0300

    Inserir listagem, busca e criação de filmes

--- moviesite/settings.py
+++ moviesite/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'staticpages.apps.StaticpagesConfig', # adicione esta linha
+    'staticpages.apps.StaticpagesConfig',
+    'movies.apps.MoviesConfig', # adicione esta linha
 ]
 
 MIDDLEWARE = [
